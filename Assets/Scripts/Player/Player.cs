7223a2a2a82ef9f643b1f3089fc06acd1da550df
Igor Pushkar <igor.pushkar@gmail.com>
2016-02-24 00:59:23 +0200

    Sliced sprites, created player prefab with basic movement, added player animation.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float moveForce = 20f, jumpForce = 700f, maxVelocity = 4f;
+
+	private Rigidbody2D body;
+	private Animator anim;
+	private bool isGrounded = true;
+
+	void Awake(){
+		InitializeVariables ();
+	}
+
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		PlayerWalkKeyboard ();
+	}
+
+	void InitializeVariables(){
+		body = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void PlayerWalkKeyboard(){
+		float forceX = 0f;
+		float forceY = 0f;
+
+		float vel = Mathf.Abs (body.velocity.x);
+
+		float h = Input.GetAxisRaw ("Horizontal");
+
+		if(h > 0){
+			if(vel < maxVelocity){
+				if(isGrounded){
+					forceX = moveForce;
+				} else {
+					forceX = moveForce * 1.1f;
+				}
+
+			}
+
+			Vector3 scale = transform.localScale;
+			scale.x = 1f;
+			transform.localScale = scale;
+
+			anim.SetBool ("Walk", true);
+		} else if (h < 0){
+			if(vel < maxVelocity){
+				if(isGrounded){
+					forceX = -moveForce;
+				} else {
+					forceX = -moveForce * 1.1f;
+				}
+			}
+
+			Vector3 scale = transform.localScale;
+			scale.x = -1f;
+			transform.localScale = scale;
+
+			anim.SetBool ("Walk", true);
+		} else if (h == 0){
+			anim.SetBool ("Walk", false);
+		}
+
+		if(Input.GetKey(KeyCode.Space)){
+			if(isGrounded){
+				isGrounded = false;
+				forceY = jumpForce;
+			}
+		}
+
+		body.AddForce (new Vector2 (forceX, forceY));
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if(collision.gameObject.tag == "Ground"){
+			isGrounded = true;
+		}
+	}
+}
